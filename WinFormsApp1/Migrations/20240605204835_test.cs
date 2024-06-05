fbe7d2b8bc658bfaae486184a7a28f7dbe2454a3
Sione Yerkovich <sioneyerkovich01@gmai.com>
2024-06-06 09:28:13 +1200

    test

---
+++ WinFormsApp1/Migrations/20240605204835_test.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WinFormsApp1.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    First_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Last_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ WinFormsApp1/MusicDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace WinFormsApp1
+{
+    internal class MusicDBContext:DbContext
+    {
+        //Code for bringing the entity model that we created for User
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            // Code for getting the connection string from user secrets
+            var builder = new ConfigurationBuilder().AddUserSecrets<MusicDBContext>();
+            var configuration = builder.Build();
+            var connectionString = configuration.GetConnectionString("Default");
+
+            //Code for connecting to SQL database
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
