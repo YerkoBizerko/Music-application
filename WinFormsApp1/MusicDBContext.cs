4240756f84f080d6e8bac03dbb2cd9fef825ee6a
Sione Yerkovich <sioneyerkovich01@gmail.com>
2025-01-15 13:14:08 +1300

    Format
    1. Migrated connection string from secrets.JSON to App.config to allow users database access when installed

--- WinFormsApp1/MusicDBContext.cs
+++ WinFormsApp1/MusicDBContext.cs
@@ -1,10 +1,10 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace WinFormsApp1
 {
@@ -13,14 +13,16 @@ namespace WinFormsApp1
         //Code for bringing the entity model that we created for User
         public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        { 
+        {
+            /*
             // Code for getting the connection string from user secrets
             var builder = new ConfigurationBuilder().AddUserSecrets<MusicDBContext>();
             var configuration = builder.Build();
             var connectionString = configuration.GetConnectionString("Default");
+            */
 
             //Code for connecting to SQL database
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString);
         }
     }
 }
